cc3c379d42eb5ce87cfcd9244cccddaa88f071df
Hassan Amin <39194813+aqeox@users.noreply.github.com>
2023-07-16 17:54:57 +0800

    modified:   app/admin/page.tsx 	new file:   app/test/page.tsx 	new file:   components/ui/card.tsx 	new file:   components/ui/input.tsx 	new file:   components/ui/label.tsx 	new file:   components/ui/radio-group.tsx 	new file:   components/ui/select.tsx 	modified:   package-lock.json 	modified:   package.json

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -11,6 +11,17 @@ import {
   XMarkIcon,
 } from "@heroicons/react/24/outline"
 
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+import { Label } from "@/components/ui/label"
+import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group"
+
 const navigation = [
   { name: "Solid", href: "#", icon: XCircleIcon, current: true },
   { name: "Outline", href: "#", icon: XCircleIcon, current: false },
@@ -230,6 +241,157 @@ export default function Example() {
                     className="mb-2 block text-sm font-medium text-gray-700"
                   >
                     Secondary Box
+                    <Card>
+                      <RadioGroup
+                        defaultValue="card"
+                        className="grid grid-cols-3 gap-4"
+                      >
+                        <Label
+                          htmlFor="card"
+                          className="flex flex-col items-center justify-between rounded-md border-2 border-muted bg-popover p-4 hover:bg-accent hover:text-accent-foreground [&:has([data-state=checked])]:border-primary"
+                        >
+                          <RadioGroupItem
+                            value="card"
+                            id="card"
+                            className="sr-only"
+                          />{" "}
+                          <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            viewBox="0 0 24 24"
+                            fill="none"
+                            stroke="currentColor"
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            strokeWidth="2"
+                            className="mb-3 h-6 w-6"
+                          >
+                            <rect width="20" height="14" x="2" y="5" rx="2" />
+                            <path d="M2 10h20" />
+                          </svg>
+                          Card
+                        </Label>
+                        <Label
+                          htmlFor="card"
+                          className="flex flex-col items-center justify-between rounded-md border-2 border-muted bg-popover p-4 hover:bg-accent hover:text-accent-foreground [&:has([data-state=checked])]:border-primary"
+                        >
+                          <RadioGroupItem
+                            value="card"
+                            id="card"
+                            className="sr-only"
+                          />{" "}
+                          <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            viewBox="0 0 24 24"
+                            fill="none"
+                            stroke="currentColor"
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            strokeWidth="2"
+                            className="mb-3 h-6 w-6"
+                          >
+                            <rect width="20" height="14" x="2" y="5" rx="2" />
+                            <path d="M2 10h20" />
+                          </svg>
+                          Card
+                        </Label>
+                        <Label
+                          htmlFor="card"
+                          className="flex flex-col items-center justify-between rounded-md border-2 border-muted bg-popover p-4 hover:bg-accent hover:text-accent-foreground [&:has([data-state=checked])]:border-primary"
+                        >
+                          <RadioGroupItem
+                            value="card"
+                            id="card"
+                            className="sr-only"
+                          />{" "}
+                          <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            viewBox="0 0 24 24"
+                            fill="none"
+                            stroke="currentColor"
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            strokeWidth="2"
+                            className="mb-3 h-6 w-6"
+                          >
+                            <rect width="20" height="14" x="2" y="5" rx="2" />
+                            <path d="M2 10h20" />
+                          </svg>
+                          Card
+                        </Label>
+                        <Label
+                          htmlFor="card"
+                          className="flex flex-col items-center justify-between rounded-md border-2 border-muted bg-popover p-4 hover:bg-accent hover:text-accent-foreground [&:has([data-state=checked])]:border-primary"
+                        >
+                          <RadioGroupItem
+                            value="card"
+                            id="card"
+                            className="sr-only"
+                          />{" "}
+                          <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            viewBox="0 0 24 24"
+                            fill="none"
+                            stroke="currentColor"
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            strokeWidth="2"
+                            className="mb-3 h-6 w-6"
+                          >
+                            <rect width="20" height="14" x="2" y="5" rx="2" />
+                            <path d="M2 10h20" />
+                          </svg>
+                          Card
+                        </Label>
+                        <Label
+                          htmlFor="card"
+                          className="flex flex-col items-center justify-between rounded-md border-2 border-muted bg-popover p-4 hover:bg-accent hover:text-accent-foreground [&:has([data-state=checked])]:border-primary"
+                        >
+                          <RadioGroupItem
+                            value="card"
+                            id="card"
+                            className="sr-only"
+                          />
+                          <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            viewBox="0 0 24 24"
+                            fill="none"
+                            stroke="currentColor"
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            strokeWidth="2"
+                            className="mb-3 h-6 w-6"
+                          >
+                            <rect width="20" height="14" x="2" y="5" rx="2" />
+                            <path d="M2 10h20" />
+                          </svg>
+                          Card
+                        </Label>
+                        <Label
+                          htmlFor="card"
+                          className="flex flex-col items-center justify-between rounded-md border-2 border-muted bg-popover p-4 hover:bg-accent hover:text-accent-foreground [&:has([data-state=checked])]:border-primary"
+                        >
+                          <RadioGroupItem
+                            value="card"
+                            id="card"
+                            className="sr-only"
+                          />{" "}
+                          <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            viewBox="0 0 24 24"
+                            fill="none"
+                            stroke="currentColor"
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            strokeWidth="2"
+                            className="mb-3 h-6 w-6"
+                          >
+                            <rect width="20" height="14" x="2" y="5" rx="2" />
+                            <path d="M2 10h20" />
+                          </svg>
+                          Card
+                        </Label>
+                      </RadioGroup>
+                    </Card>
                   </label>
                 </div>
               </div>
---
+++ app/test/page.tsx
@@ -0,0 +1,47 @@
+"use client"
+
+import React from "react"
+
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
+import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group"
+import { Icons } from "@/components/icons"
+
+export default function page() {
+  return (
+    <Card>
+      <CardContent className="grid gap-6">
+        <RadioGroup defaultValue="card" className="grid grid-cols-3 gap-4">
+          <Label
+            htmlFor="card"
+            className="flex flex-col items-center justify-between rounded-md border-2 border-muted bg-popover p-4 hover:bg-accent hover:text-accent-foreground [&:has([data-state=checked])]:border-primary"
+          >
+            <RadioGroupItem value="card" id="card" className="sr-only" />
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              className="mb-3 h-6 w-6"
+            >
+              <rect width="20" height="14" x="2" y="5" rx="2" />
+              <path d="M2 10h20" />
+            </svg>
+            Card
+          </Label>
+        </RadioGroup>
+      </CardContent>
+    </Card>
+  )
+}
