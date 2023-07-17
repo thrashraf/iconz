7ad5662706aa15da90364ce822a1f25b5b745660
Hassan Amin <39194813+aqeox@users.noreply.github.com>
2023-07-17 13:12:42 +0800

    modified:   app/page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -251,7 +251,7 @@ export default function Example() {
                         viewBox="0 0 24 24"
                         stroke-width="1.5"
                         stroke="currentColor"
-                        class="h-6 w-6"
+                        className="h-6 w-6"
                       >
                         <path
                           stroke-linecap="round"
@@ -276,7 +276,7 @@ export default function Example() {
                         viewBox="0 0 24 24"
                         stroke-width="1.5"
                         stroke="currentColor"
-                        class="h-6 w-6"
+                        className="h-6 w-6"
                       >
                         <path
                           stroke-linecap="round"
@@ -301,7 +301,7 @@ export default function Example() {
                         viewBox="0 0 24 24"
                         stroke-width="1.5"
                         stroke="currentColor"
-                        class="w-6 h-6"
+                        className="h-6 w-6"
                       >
                         <path
                           stroke-linecap="round"
@@ -326,7 +326,7 @@ export default function Example() {
                         viewBox="0 0 24 24"
                         stroke-width="1.5"
                         stroke="currentColor"
-                        class="w-6 h-6"
+                        className="h-6 w-6"
                       >
                         <path
                           stroke-linecap="round"
@@ -351,7 +351,7 @@ export default function Example() {
                         viewBox="0 0 24 24"
                         stroke-width="1.5"
                         stroke="currentColor"
-                        class="w-6 h-6"
+                        className="h-6 w-6"
                       >
                         <path
                           stroke-linecap="round"
@@ -376,7 +376,7 @@ export default function Example() {
                         viewBox="0 0 24 24"
                         stroke-width="1.5"
                         stroke="currentColor"
-                        class="w-6 h-6"
+                        className="h-6 w-6"
                       >
                         <path
                           stroke-linecap="round"
