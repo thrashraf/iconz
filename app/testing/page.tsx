71c394b94b5210b652e43089c6c0e88071d43a9c
evoaiman <aiman_saharudin@yahoo.com>
2023-07-19 22:36:14 +0800

    modified:   app/testing/page.tsx

--- app/testing/page.tsx
+++ app/testing/page.tsx
@@ -36,75 +36,94 @@ export default function page() {
         </AccordionItem>
       </Accordion> */}
       <div className="bg-white">
-        <main className="container mx-auto px-4 py-8">
-          <h1 className="mb-4 text-4xl font-bold"></h1>
-          <p className="mb-8 text-gray-600">Dress</p>
+        <main className="container mx-auto flex flex-row px-4 py-8">
+          <div className=" sidebar h-auto min-h-[140px] w-64 overflow-scroll">
+            <ul className="flex flex-col gap-4">
+              <li className="font-bold">Shoes</li>
+              <li className="font-bold">Shoes</li>
+              <li className="font-bold">Shoes</li>
+              <li className="font-bold">Shoes</li>
+            </ul>
 
-          {/* Product Cards */}
-          <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
-            {/* Single Product Card */}
-            <div className="rounded-lg bg-white shadow-md">
-              <img
-                src="/1.webp"
-                alt="Product 1"
-                className="h-full w-full  object-cover"
-              />
-            </div>
-            <div className="rounded-lg bg-white shadow-md">
-              <img
-                src="/1.webp"
-                alt="Product 1"
-                className="h-full w-full  object-cover"
-              />
-              <div className="p-4">
-                <h3 className="mb-2 text-xl font-semibold">
-                  WHITE OSTRICH DRESS
-                </h3>
-                <p className="text-gray-600">3 Colour</p>
-                <div className="mt-4">
-                  <span className="text-gray-500">RM2,899.00</span>
-                </div>
-              </div>
-            </div>
-            <div className="rounded-lg bg-white shadow-md">
-              <img
-                src="/1.webp"
-                alt="Product 1"
-                className="h-full w-full  object-cover"
-              />
-              <div className="p-4">
-                <h3 className="mb-2 text-xl font-semibold">
-                  WHITE OSTRICH DRESS
-                </h3>
-                <p className="text-gray-600">3 Colour</p>
-                <div className="mt-4">
-                  <span className="text-gray-500">RM2,899.00</span>
-                </div>
-              </div>
-            </div>
-            <div className="rounded-lg bg-white shadow-md">
-              <img
-                src="/1.webp"
-                alt="Product 1"
-                className="h-full w-full  object-cover"
-              />
-              <div className="p-4">
-                <h3 className="mb-2 text-xl font-semibold">
-                  WHITE OSTRICH DRESS
-                </h3>
-                <p className="text-gray-600">3 Colour</p>
-                <div className="mt-4">
-                  <span className="text-gray-500">RM2,899.00</span>
-                </div>
-              </div>
-            </div>
-            {/* Add more product cards as needed */}
+            <ul className="gender border-t border-[#E5E5E5]">
+              <li className="font-bold">
+                <input type="checkbox" name="" id="" /> Shoes
+              </li>
+              <li className="font-bold">Shoes</li>
+            </ul>
           </div>
-          <div className="p-4">
-            <h3 className="mb-2 text-xl font-semibold">WHITE OSTRICH DRESS</h3>
-            <p className="text-gray-600">3 Colour</p>
-            <div className="mt-4">
-              <span className="text-gray-500">RM2,899.00</span>
+          <div className="content w-full">
+            <h1 className="mb-4 text-4xl font-bold"></h1>
+            <p className="mb-8 text-gray-600">Dress</p>
+
+            {/* Product Cards */}
+            <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+              {/* Single Product Card */}
+              <div className="rounded-lg bg-white shadow-md">
+                <img
+                  src="/1.webp"
+                  alt="Product 1"
+                  className="h-full w-full  object-cover"
+                />
+              </div>
+              <div className="rounded-lg bg-white shadow-md">
+                <img
+                  src="/1.webp"
+                  alt="Product 1"
+                  className="h-full w-full  object-cover"
+                />
+                <div className="p-4">
+                  <h3 className="mb-2 text-xl font-semibold">
+                    WHITE OSTRICH DRESS
+                  </h3>
+                  <p className="text-gray-600">3 Colour</p>
+                  <div className="mt-4">
+                    <span className="text-gray-500">RM2,899.00</span>
+                  </div>
+                </div>
+              </div>
+              <div className="rounded-lg bg-white shadow-md">
+                <img
+                  src="/1.webp"
+                  alt="Product 1"
+                  className="h-full w-full  object-cover"
+                />
+                <div className="p-4">
+                  <h3 className="mb-2 text-xl font-semibold">
+                    WHITE OSTRICH DRESS
+                  </h3>
+                  <p className="text-gray-600">3 Colour</p>
+                  <div className="mt-4">
+                    <span className="text-gray-500">RM2,899.00</span>
+                  </div>
+                </div>
+              </div>
+              <div className="rounded-lg bg-white shadow-md">
+                <img
+                  src="/1.webp"
+                  alt="Product 1"
+                  className="h-full w-full  object-cover"
+                />
+                <div className="p-4">
+                  <h3 className="mb-2 text-xl font-semibold">
+                    WHITE OSTRICH DRESS
+                  </h3>
+                  <p className="text-gray-600">3 Colour</p>
+                  <div className="mt-4">
+                    <span className="text-gray-500">RM2,899.00</span>
+                  </div>
+                </div>
+              </div>
+              {/* Add more product cards as needed */}
+            </div>
+            <div className="p-4">
+              <h3 className="mb-2 text-xl font-semibold">
+                WHITE OSTRICH DRESS
+              </h3>
+              <p className="text-gray-600">3 Colour</p>
+              <div className="mt-4">
+                <span className="text-gray-500">RM2,899.00</span>
+              </div>
             </div>
           </div>
         </main>
